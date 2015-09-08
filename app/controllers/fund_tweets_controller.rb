7576fd693aee2f50fda79933f3e0c6732a4acb13
Loraine <loraine@lorainekv.com>
2015-09-08 13:59:36 -0700

    Retweet currently just posts a 'Look out, world' message.

--- app/controllers/fund_tweets_controller.rb
+++ app/controllers/fund_tweets_controller.rb
@@ -4,4 +4,9 @@ class FundTweetsController < ApplicationController
     @search_results = twitter_client.search("tech conference scholarship").take(5)
     render json: {}
   end
+
+  def retweet
+    twitter_client.update("Look out, world!")
+    render json: {}
+  end
 end
