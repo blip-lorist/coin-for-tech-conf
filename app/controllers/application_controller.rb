968995415ca27ee09e0bfbe9a84563417531224d
Loraine <loraine@lorainekv.com>
2015-09-08 10:58:31 -0700

    Relocated the twitter client initializer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 require "twitter"
-require "twit_init"
 
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
 
-  def twit_init
-    @twit_init || TwitInit.new
-  end
 end
