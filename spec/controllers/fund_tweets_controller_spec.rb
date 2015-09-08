cb040a7fca9e6bcdccc522a37a9d28e839d6d4df
Loraine <loraine@lorainekv.com>
2015-09-08 13:10:57 -0700

    VCR config.

--- spec/controllers/fund_tweets_controller_spec.rb
+++ spec/controllers/fund_tweets_controller_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
+require 'vcr_setup'
 
 RSpec.describe FundTweetsController, type: :controller do
 
   describe "GET fund_tweets#search" do
+    VCR.use_cassette "twitter_search_results" do
 
-    it "returns JSON" do
-      VCR.use_cassette 'twitter_search_results' do
+      it "returns JSON" do
         get :search
         expect(response.header['Content-Type']).to include 'application/json'
       end
-    end
 
-    it "returns tweets after a successful search query" do
-      VCR.use_cassette 'twitter_search_results' do
+      it "returns tweets after a successful search query" do
         get :search
         expect(assigns(:response).count).to eq(1)
       end
