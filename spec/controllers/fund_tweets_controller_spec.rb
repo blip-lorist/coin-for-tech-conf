8d130744665c6a0e03ec160530e0047dbcd669b7
Loraine <loraine@lorainekv.com>
2015-09-08 13:30:01 -0700

    Hooray, got my first VCR cassette. Hope the tape doesn't get jammed.

--- spec/controllers/fund_tweets_controller_spec.rb
+++ spec/controllers/fund_tweets_controller_spec.rb
@@ -4,14 +4,16 @@ require 'vcr_setup'
 RSpec.describe FundTweetsController, type: :controller do
 
   describe "GET fund_tweets#search" do
-    VCR.use_cassette "twitter_search_results" do
 
-      it "returns JSON" do
+    it "returns JSON" do
+      VCR.use_cassette "twitter_search_results" do
         get :search
         expect(response.header['Content-Type']).to include 'application/json'
       end
+    end
 
-      it "returns tweets after a successful search query" do
+    it "returns tweets after a successful search query" do
+      VCR.use_cassette "twitter_search_results" do
         get :search
         expect(assigns(:response).count).to eq(1)
       end
