065aaaafc0770579b6593df2bcb2475e155551ac
Loraine <loraine@lorainekv.com>
2015-09-08 00:02:26 -0700

    Wrote initial search twitter spec.

--- spec/controllers/fund_tweets_controller_spec.rb
+++ spec/controllers/fund_tweets_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe FundTweetsController, type: :controller do
 
+  describe "GET fund_tweets#search" do
+    let (:twitter_params) {{search_term: "tech conference scholarship"}}
+
+    it "receives search results from the Twitter API" do
+      VCR.use_cassette 'twitter_search_results' do
+        get :search, twitter_params
+        expect(assigns(:results).count).to eq(3)
+      end
+    end
+  end
+
+
+
+
 end
