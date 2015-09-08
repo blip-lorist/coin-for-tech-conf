85d73cb6bea1877766c0481ca3a9fb7c0c01cf11
Loraine <loraine@lorainekv.com>
2015-09-08 16:37:45 -0700

    App now retweets things. Twitter gem is awesome, and it will auto-prevent you from retweeting duplicates. Need to create a rescue for this.

--- app/controllers/fund_tweets_controller.rb
+++ app/controllers/fund_tweets_controller.rb
@@ -2,11 +2,25 @@ class FundTweetsController < ApplicationController
 
   def search
     @search_results = twitter_client.search("tech conference scholarship").take(5)
-    render json: {}
+    render json: @search_results
   end
 
   def retweet
-    @post = twitter_client.update("Look out, world!")
-    render json: {}
+    search
+    @search_results.each do |tweet|
+      twitter_client.retweet(tweet)
+    end
   end
+
+  private
+
+  # def prep_results(results)
+  #   retweet_ids = []
+  #
+  #   @search_results.each do |tweet|
+  #     retweet_ids << tweet.id
+  #   end
+  #   retweet_ids
+  # end
+
 end
