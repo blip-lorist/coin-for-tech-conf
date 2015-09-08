084b1f342ed836bc338667134d9fbdd8b810dc27
Loraine <loraine@lorainekv.com>
2015-09-08 00:19:00 -0700

    First green spec about it returning a json object."

--- app/controllers/fund_tweets_controller.rb
+++ app/controllers/fund_tweets_controller.rb
@@ -1,5 +1,5 @@
 class FundTweetsController < ApplicationController
   def search
-
+    render json: {}
   end
 end
--- spec/controllers/fund_tweets_controller_spec.rb
+++ spec/controllers/fund_tweets_controller_spec.rb
@@ -5,11 +5,17 @@ RSpec.describe FundTweetsController, type: :controller do
   describe "GET fund_tweets#search" do
     let (:twitter_params) {{search_term: "tech conference scholarship"}}
 
-    it "receives search results from the Twitter API" do
-      VCR.use_cassette 'twitter_search_results' do
-        get :search, twitter_params
-        expect(assigns(:results).count).to eq(3)
-      end
+    it "returns JSON" do
+      get :search, twitter_params
+      expect(response.header['Content-Type']).to include 'application/json'
     end
+
+
+    # it "receives search results from the Twitter API" do
+    #   VCR.use_cassette 'twitter_search_results' do
+    #     get :search, twitter_params
+    #
+    #   end
+    # end
   end
 end
