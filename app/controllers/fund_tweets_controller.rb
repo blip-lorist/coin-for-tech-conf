d443acef1fd71bb230bff1b400fb0b4b5014b4fb
Loraine <loraine@lorainekv.com>
2015-09-09 15:38:26 -0700

    Moved search into a private method. Retweet must now render something.

--- app/controllers/fund_tweets_controller.rb
+++ app/controllers/fund_tweets_controller.rb
@@ -1,12 +1,5 @@
 class FundTweetsController < ApplicationController
 
-  def search
-    ## Since this will be automated, I'm going to just snag a single result at
-    ## a time in an attempt to focus on quality of the retweet vs quantity.
-    @search_results = twitter_client.search("tech conference scholarship").take(1)
-    render json: @search_results
-  end
-
   def retweet
     # Snag the search results
     search
@@ -19,12 +12,22 @@ class FundTweetsController < ApplicationController
         next
       end
     end
+    render json: {}
   end
 
   def cron_test
     rando_string = (0...8).map { (65 + rand(26)).chr }.join
     @post = twitter_client.update(rando_string)
     render json: {}
+  end
+
+  private
+
+  def search
+    ## Since this will be automated, I'm going to just snag a single result at
+    ## a time in an attempt to focus on quality of the retweet vs quantity.
+    @search_results = twitter_client.search("conference scholarship apply OR win tech OR technology", result_type: "recent").take(1)
+    # render json: @search_results
   end
 
 end
--- spec/controllers/fund_tweets_controller_spec.rb
+++ spec/controllers/fund_tweets_controller_spec.rb
@@ -5,22 +5,21 @@ RSpec.describe FundTweetsController, type: :controller do
 
   describe "GET fund_tweets#search" do
 
-    it "returns JSON" do
-      VCR.use_cassette "twitter_search_results" do
-        get :search
-        expect(response.header['Content-Type']).to include 'application/json'
-      end
-    end
-
     it "returns a tweet after a successful search query" do
       VCR.use_cassette "twitter_search_results" do
-        get :search
+        controller.send(:search)
         expect(assigns(:search_results).count).to eq(1)
       end
     end
   end
 
   describe "POST fund_tweets#retweet" do
+    it "returns a JSON object" do
+      VCR.use_cassette "retweets" do
+        get :retweet
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+    end
 
     context "when a unique tweet is found" do
       it "retweets it" do
