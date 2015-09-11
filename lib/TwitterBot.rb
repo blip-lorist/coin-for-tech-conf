2e9ff01dea91c6a154299bc82d2593b768fdecf4
Loraine <loraine@lorainekv.com>
2015-09-11 02:09:30 -0700

    Created a method that filters out tweets that are too similar. Thanks similar_text gem.

--- lib/TwitterBot.rb
+++ lib/TwitterBot.rb
@@ -17,14 +17,13 @@ class TwitterBot
   end
 
   def retweet
-    unless search == nil
+    unless filter(search) == nil
       post_stuff
     end
   end
 
   def post_stuff
-    # Retweet
-    @search_results.each do |tweet|
+    @filtered_results.each do |tweet|
       begin
         twitter_client.retweet(tweet)
       rescue Twitter::Error # Lazy guard clause
@@ -41,11 +40,51 @@ class TwitterBot
 
   def search
     # Snag the five most recent relevant tweets
-    first_search = twitter_client.search("conference OR conf apply OR win tech OR technology scholarship -filter:retweets -filter:mentions").take(10)
-    second_search = twitter_client.search("conference OR conf tech OR technology free OR reduced ticket OR admission -filter:retweets -filter:mentions").take(10)
+    first_search = twitter_client.search("conference OR conf apply OR win tech OR technology scholarship -filter:retweets -filter:mentions").take(5)
+    second_search = twitter_client.search("conference OR conf tech OR technology free OR reduced ticket OR admission -filter:retweets -filter:mentions").take(5)
     third_search = twitter_client.search("from:diversity_conf scholarship").take(5)
 
     @search_results = first_search + second_search + third_search
   end
 
+  def filter(results)
+
+    duplicates = []
+    uniques = []
+    percentages = []
+
+    # Comparisons can't happen with less than two results
+    until @search_results.length <= 1
+
+      # Compare the first tweet to all results
+      for i in 1...@search_results.length
+        check = (@search_results.first.text).similar(@search_results[i].text)
+        percentages << check
+      end
+
+      # Sort the first tweet based on % similarity
+      if percentages.max > 80
+        duplicates << @search_results.shift
+      else
+        uniques << @search_results.shift
+      end
+
+      # Reset the percentages for the next iteration
+      percentages = []
+    end
+
+    #Filtering that one leftover tweet
+    uniques.each do |unique|
+      unless @search_results.length == 0
+        check = (@search_results.first.text).similar(unique.text)
+        if check > 80
+          duplicates << @search_results.shift
+        end
+      end
+    end
+
+    uniques << @search_results.shift
+
+    @filtered_results = uniques
+  end
 end
