2cb0677136eb453a90026bb4cb1c9912cd34f90e
Loraine <loraine@lorainekv.com>
2016-02-29 01:21:07 -0800

    Added stack-specific scholarship searches.

--- twitter_bot.rb
+++ twitter_bot.rb
@@ -15,14 +15,18 @@ class TwitterBot
   end
 
   def search
-    # Snag five most recent tweets within three search groups
-    first_search = twitter_client.search("conference OR conf apply OR win tech OR technology scholarship").take(5)
+    # Snag three recent tweets within five search groups
+    first_search = twitter_client.search("conference OR conf apply OR win tech OR technology scholarship").take(3)
 
-    second_search = twitter_client.search("conference OR conf tech OR technology free OR reduced ticket OR admission").take(5)
+    second_search = twitter_client.search("conference OR conf tech OR technology free OR reduced ticket OR admission").take(3)
 
-    third_search = twitter_client.search("underrepresented OR minority tech OR technology summit OR scholarship").take(5)
+    third_search = twitter_client.search("underrepresented OR minority tech OR technology summit OR scholarship").take(3)
 
-    @search_results = first_search + second_search + third_search
+    fourth_search = twitter_client.search("scholarship apply ruby OR python OR javascript OR java OR php -clifton -monty").take(3)
+    
+    fifth_search = twitter_client.search("scholarship apply asp.net OR angularjs OR rails OR django OR meteor OR laravel OR codeigniter OR reactjs OR nodejs").take(3)
+
+    @search_results = first_search + second_search + third_search + fourth_search + fifth_search
   end
   
   def retweet
